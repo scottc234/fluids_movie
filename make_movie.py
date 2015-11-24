1eef37f42bc5772bdfa5db9cac2c3947aed6c07b
Scott <scott.clark@monash.edu>
2015-11-24 13:55:01 +1100

    Add argument parsing

--- make_movie.py
+++ make_movie.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
+import argparse
+import netCDF4 as nc
 
 """
 
-This script is a trial and makes a fluid animation using netcdf data.
+#This script is a trial and makes a fluid animation using netcdf data.
 """
 
 def main():
+    
+    args = argparse.ArgumentParser()
+    parser.add_argument('input_file', help="Input file containing our data")
+    parser.add_argument('field', help="Data field to animate")
+    
+    args = parser.parse_args()
+
     print("I'm in the main function")
     return True
 
