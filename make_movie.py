50003f7ea050d658efca7d4d10468e79a80d28fe
Scott <scott.clark@monash.edu>
2015-11-24 15:55:20 +1100

    Change gitignore and fluids images done

--- make_movie.py
+++ make_movie.py
@@ -2,8 +2,11 @@
 
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
 import argparse
 import netCDF4 as nc
+
 
 """
 
@@ -12,12 +15,34 @@ import netCDF4 as nc
 
 def main():
     
-    args = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help="Input file containing our data")
-    parser.add_argument('field', help="Data field to animate")
-    
+    parser.add_argument('field_name', help="Data field to animate")
     args = parser.parse_args()
 
+    #This is the same as saying:
+    f = nc.Dataset(args.input_file)
+    vorticity = f.variables['vorticity_z']
+    vorticity = vorticity[:]
+    for n in range(vorticity.shape[0]):
+      img =  plt.imshow(vorticity[n,0,:,:])
+     # images.append([img])
+    #To show plot immediately without saving:
+    #plt.show()
+    #save plot as image:
+     #  plt.savefig('vorticity'+str(n)+'.png')
+      plt.savefig('vorticity%03d.png'%n)		#alternative
+
+    #Vorticity is a multi dimensional array. Dimensions are:
+    #Time, pressure level, longitude, latitude.
+    #This code will be removed.
+    #import pdb
+    #pdb.set_trace()
+
+
+    f.close()
+    #vort_animation = animation.ArtistAnimation(fig,images,interval=20)
+    #plt.show()
     print("I'm in the main function")
     return True
 
