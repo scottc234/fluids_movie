3dd5afcf4fe4e6b84a4ea2f18d3e68932bafa85a
Scott <scott.clark@monash.edu>
2015-11-24 13:31:36 +1100

    Basic main function done.

---
+++ make_movie.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+"""
+
+This script is a trial and makes a fluid animation using netcdf data.
+"""
+
+def main():
+    print("I'm in the main function")
+    return True
+
+if __name__ == "__main__":
+    sys.exit(main())
